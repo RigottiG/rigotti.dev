84f707f89e4a5a8698a54bbc3625e8567476b466
Guilherme Rigotti <ggrigotti@gmail.com>
2023-03-02 12:43:03 -0300

    Update layout and page styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,34 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="mx-auto w-full max-w-4xl font-mono">
-      <div className="grid grid-cols-1 place-content-center items-center gap-8 md:grid-cols-2">
+      <div className="grid grid-cols-1 place-content-center items-center gap-8 border-b pb-4 md:grid-cols-2 md:border-none md:pb-0">
         <Image
           src="https://www.github.com/rigottig.png"
           width={280}
           height={280}
           alt="My profile photo from GitHub"
-          className="place-self-center"
+          className="place-self-center object-cover md:place-self-auto"
         />
         <div className="space-y-4">
-          <h2 className="text-center text-2xl font-semibold leading-9">
-            👋 Hey, I&apos;m Guilherme Rigotti also known as{' '}
+          <h1 className="mx-auto max-w-sm text-center text-2xl font-semibold leading-9 md:max-w-md">
+            <span className="hidden md:inline-block">👋</span> Hey, I&apos;m
+            Guilherme Rigotti <br /> also known as{' '}
             <a
               className="text-blue-600 underline visited:text-purple-600 hover:text-blue-800"
               href="https://www.github.com.br/rigottig"
             >
               @rigottig
             </a>
-          </h2>
-          <p className="text-center font-medium">
+          </h1>
+          <p className="text-center text-lg font-medium">
             I&apos;m a <strong>Software Engineer</strong> from{' '}
             <strong>Brazil</strong> 🇧🇷
           </p>
         </div>
+      </div>
+
+      <div className="mt-4 md:mt-14 md:pl-10">
+        <h3 className="text-2xl font-semibold">✨ Latest posts</h3>
       </div>
     </main>
   )
