a995c70bd89e27d4c88859c96d8535b8a07566ec
Guilherme Rigotti <ggrigotti@gmail.com>
2023-03-02 15:13:56 -0300

    Add PostCard component

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,15 +1,33 @@
+import Link from 'next/link'
 import { ImLinkedin, ImGithub } from 'react-icons/im'
 import { SiInstagram } from 'react-icons/si'
 import { TfiEmail } from 'react-icons/tfi'
+
 export function Footer() {
   return (
-    <footer className="container mx-auto flex flex-col items-center gap-5">
-      <div className="flex gap-5">
-        <ImLinkedin />
-        <ImGithub />
-        <SiInstagram />
-        <TfiEmail />
-      </div>
+    <footer className="container mx-auto mb-4 flex flex-col items-center space-y-5">
+      <ul className="flex gap-5">
+        <li>
+          <Link href="https://linkedin.com/in/rigottig">
+            <ImLinkedin className="duration-200 hover:scale-110" size={22} />
+          </Link>
+        </li>
+        <li>
+          <Link href="https://github.com/rigottig">
+            <ImGithub className="duration-200 hover:scale-110" size={22} />
+          </Link>
+        </li>
+        <li>
+          <Link href="https://instagram.com/rigottig">
+            <SiInstagram className="duration-200 hover:scale-110" size={22} />
+          </Link>
+        </li>
+        <li>
+          <Link type="email" href="mailto:ggrigoti@gmail.com">
+            <TfiEmail className="duration-200 hover:scale-110" size={23} />
+          </Link>
+        </li>
+      </ul>
       <p className="text-xs text-gray-400">
         &copy; Guilherme Rigotti - {new Date().getFullYear()}
       </p>
