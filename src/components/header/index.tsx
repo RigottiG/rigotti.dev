262584efa4b47355f5cae1ed075628cca7766103
Guilherme Rigotti <ggrigotti@gmail.com>
2023-03-03 14:07:00 -0300

    style: remove menu box shadow

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,31 +4,19 @@ export function Header() {
   return (
     <header className="mx-auto flex flex-col items-center pt-4 pb-8">
       <nav className="space-x-3">
-        <Link
-          className="rounded-t-md px-2 duration-200 hover:text-white hover:shadow-[0px_-25px_0px_inset_#000]"
-          href="/"
-        >
+        <Link className="duration-200 hover:text-gray-700" href="/">
           Home
         </Link>
         <span>|</span>
-        <Link
-          className="rounded-t-md px-2 duration-200 hover:text-white hover:shadow-[0px_-25px_0px_inset_#000]"
-          href="/blog"
-        >
+        <Link className="duration-200 hover:text-gray-700" href="/blog">
           Blog
         </Link>
         <span>|</span>
-        <Link
-          className="rounded-t-md px-2 duration-200 hover:text-white hover:shadow-[0px_-25px_0px_inset_#000]"
-          href="/about"
-        >
+        <Link className="duration-200 hover:text-gray-700" href="/about">
           About
         </Link>
         <span>|</span>
-        <Link
-          className="rounded-t-md px-2 duration-200 hover:text-white hover:shadow-[0px_-25px_0px_inset_#000]"
-          href="/contact"
-        >
+        <Link className="duration-200 hover:text-gray-700" href="/contact">
           Contact
         </Link>
       </nav>
