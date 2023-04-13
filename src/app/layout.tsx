be7301083173260920a6d507a21e506cd9d62e81
Guilherme Rigotti <ggrigotti@gmail.com>
2023-04-13 14:06:16 -0300

    revert: remove unnecessary eslint rules

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable no-undef */
-/* eslint-disable camelcase */
 import { Footer } from '@/components/footer'
 import { Header } from '@/components/header'
 import { Montserrat, Open_Sans } from 'next/font/google'
