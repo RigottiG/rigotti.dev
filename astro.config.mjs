1ff5e2cc3be7d5f6284c4dbdeb48103c51ea8254
Guilherme Rigotti <ggrigotti@gmail.com>
2026-02-20 16:15:41 -0300

    update tailwind and post collections

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,15 @@
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite'
 import vercel from '@astrojs/vercel'
 import sitemap from '@astrojs/sitemap'
 import mdx from '@astrojs/mdx'
 
 export default defineConfig({
   site: 'https://rigotti.dev',
-  integrations: [
-    tailwind({
-      applyInputStylesToLayout: false,
-    }),
-    sitemap(),
-    mdx(),
-  ],
+  integrations: [sitemap(), mdx()],
   output: 'static',
   adapter: vercel(),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
